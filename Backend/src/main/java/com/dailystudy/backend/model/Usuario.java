9daeebd5120e056eaeee703a8e539c87613a52f6
PE-zc <pauloernesto2004@gmail.com>
2026-04-25 23:20:36 -0300

    Alterando a lógica para o banco relacional

--- Backend/src/main/java/com/dailystudy/backend/model/Usuario.java
+++ Backend/src/main/java/com/dailystudy/backend/model/Usuario.java
@@ -4,17 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.persistence.*;
 
-@Document(collection = "usuario")
+@Entity
+@Table(name = "usuarios")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Usuario {
-    @Id
     //O Id garante que cada usuario tenha um registro proprio
-    private String id;
+    @Id
+    //Um valor para o Id é gerado para cada registro inserido no banco.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //Long é utilizado para numeros inteiros, que perfomam melhor no banco
+    private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String senha;
 }
