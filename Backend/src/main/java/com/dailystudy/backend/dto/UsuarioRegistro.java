0d6a64a0cfaf7ad9fbc296202f379de84c0b3d18
PE-zc <pauloernesto2004@gmail.com>
2026-04-26 20:34:54 -0300

    Alterando o DTO

--- Backend/src/main/java/com/dailystudy/backend/dto/UsuarioRegistro.java
+++ Backend/src/main/java/com/dailystudy/backend/dto/UsuarioRegistro.java
@@ -1,12 +1,24 @@
 package com.dailystudy.backend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-
-//Aqui nao precisamos colocar o Id pois o MongoDB gera sozinho
 @Data
 public class UsuarioRegistro {
+
+    @NotBlank(message = "Insira um nome de usuário")
     private String username;
+
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            message = "Insira um email válido")
     private String email;
+
+    @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
+    @NotBlank(message = "A senha é obrigatória")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+                message = "A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais")
     private String senha;
 }
