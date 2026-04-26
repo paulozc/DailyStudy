11450ca6b114b5d0a017ebc5139f9954ec9e6732
PE-zc <pauloernesto2004@gmail.com>
2026-04-26 11:53:14 -0300

    Alterando o Repository

--- Backend/src/main/java/com/dailystudy/backend/repository/UsuarioRepository.java
+++ Backend/src/main/java/com/dailystudy/backend/repository/UsuarioRepository.java
@@ -1,11 +1,16 @@
 package com.dailystudy.backend.repository;
 
 import com.dailystudy.backend.model.Usuario;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    //Aqui o JPA funciona como as queries do SQL: SELECT * FROM usuarios WHERE email = ?
     Optional<Usuario> findByEmail(String email);
+
+    //Aqui da mesma forma porém trocando pelo username: SELECT * FROM usuarios WHERE username = ?
     Optional<Usuario> findByUsername(String username);
 }
